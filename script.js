7adbc7081dc422404b5545298e7e516982fbd539
owentr1369 <tamvaa1306@gmail.com>
2022-06-18 23:47:43 +0700

    setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const items = [
+  { name: "Bike", price: 200 },
+  { name: "TV", price: 300 },
+  { name: "Album", price: 50 },
+  { name: "Book", price: 20 },
+  { name: "Keyboard", price: 150 },
+];
